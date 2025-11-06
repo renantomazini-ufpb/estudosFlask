8661420d24c9eafea695ef62b884a4367f97227f
Renan UFPB <renantomazini@sti.ufpb.br>
2025-11-06 17:36:04 -0300

    Status adicionado

--- toDoList/app.py
+++ toDoList/app.py
@@ -8,15 +8,19 @@ registros = []
 def home():
     return render_template("layout.html")
 
-@app.route('/lista', methods = ['GET','POST'])
+@app.route('/lista', methods = ['GET','POST', 'UPDATE'])
 def listar():
     #print(registros)
     if request.method == "POST":
         if request.form.get("tarefaNome") and request.form.get("tarefaDescricao") and request.form.get("tarefaPrioridade"):
-            registros.append({'tarefaNome': request.form.get('tarefaNome'),"tarefaDescricao" : request.form.get('tarefaDescricao'),"tarefaPrioridade": request.form.get("tarefaPrioridade")})
+            registros.append({'tarefaNome': request.form.get('tarefaNome'),"tarefaDescricao" : request.form.get('tarefaDescricao'),"tarefaPrioridade": request.form.get("tarefaPrioridade"), "status": True})
             #print(registros)
 
     if request.method == "GET":
         return render_template('todo.html', registros=registros)
+    
+    if request.method == "UPDATE":
+        
+        pass
 
     return render_template('todo.html', registros=registros)
